9dd207d9a2a2a4f6abe3ef658e3ac431472b6bf8
WANDJI TCHAMNDA Steve <wastevus@gmail.com>
2015-06-19 16:03:15 -0400

    Gruntfile created.

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            src: ['js/**/*.js', 'test/**/*.js', '!node_modules/**/*.*'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            }
+        },
+        watch: {
+            js: {
+                files: ['**/*.js', '!**/node_modules/**'],
+                tasks: ['lint', 'test']
+            }
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: "./src/main",
+                    paths: {
+                        circuits: "."
+                    },
+                    name: "circuits",
+                    out: "circuits-min.js"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('lint', ['jshint']);
+    grunt.registerTask('test', ['karma']);
+    grunt.registerTask('compile', ['requirejs']);
+    grunt.registerTask('default', ['lint', 'test']);
+
+};
