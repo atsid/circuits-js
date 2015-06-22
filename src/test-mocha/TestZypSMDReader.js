dad7a7a32f936b2e1229d472a0e3aa954133412e
WANDJI TCHAMNDA Steve <wastevus@gmail.com>
2015-06-22 15:22:28 -0400

    Assertions converted to chai format.

--- src/test-mocha/TestZypSMDReader.js
+++ src/test-mocha/TestZypSMDReader.js
@@ -78,7 +78,7 @@ define(["circuits/ZypSMDReader",
     describe("Test the Zyp SMD reader's functions", function () {
                 
         it("Verify if the case service matches yhe reader-resolved object", function () {
-            assertEquals(myCaseService, resolvedCaseService);
+            assert.equal(myCaseService, resolvedCaseService);
         });   
 
 
@@ -86,24 +86,24 @@ define(["circuits/ZypSMDReader",
         
         
         it("Verify non-function variables are instantiated correctly by the constructor", function () {
-            assertNotEquals(undefined, this.myReader);
-            assertEquals(resolvedCaseService, caseServiceForReader);
-            assertTrue(this.myReader.smd.resolved);
-            assertTrue(this.myReader.smd.resolvedProperties);
+            assert.notEqual(undefined, this.myReader);
+            assert.equal(resolvedCaseService, caseServiceForReader);
+            assert.isTrue(this.myReader.smd.resolved);
+            assert.isTrue(this.myReader.smd.resolvedProperties);
         });
 
 
         
         
         it("Verify if the id field of the used test schema is equal to the id returned by the getSchemaId() function", function () {
-            assertEquals(myCaseService.id, this.myReader.getSchemaId()); 
+            assert.equal(myCaseService.id, this.myReader.getSchemaId()); 
         });
 
 
         
         
         it("Verify if the target field of the used test schema is equal to the path returned by the getRootPath() function", function () {
-            assertEquals(myCaseService.target, this.myReader.getRootPath());
+            assert.equal(myCaseService.target, this.myReader.getRootPath());
         });
 
 
@@ -113,12 +113,12 @@ define(["circuits/ZypSMDReader",
         it("Verify if the names of each of the methods from the schema is returned in the array from the getMethodNames() function. The order of the methods is also preserved.", function () {
             var methodNames = ["readCaseList", "readCase", "readCaseNoteList", "deleteCaseNote", "readRawPDF"],
             methodNameArray = this.myReader.getMethodNames();          
-            assertEquals(5, methodNameArray.length);
-            assertEquals(methodNames[0], methodNameArray[0]);
-            assertEquals(methodNames[1], methodNameArray[1]);
-            assertEquals(methodNames[2], methodNameArray[2]);
-            assertEquals(methodNames[3], methodNameArray[3]);
-            assertEquals(methodNames[4], methodNameArray[4]);
+            assert.equal(5, methodNameArray.length);
+            assert.equal(methodNames[0], methodNameArray[0]);
+            assert.equal(methodNames[1], methodNameArray[1]);
+            assert.equal(methodNames[2], methodNameArray[2]);
+            assert.equal(methodNames[3], methodNameArray[3]);
+            assert.equal(methodNames[4], methodNameArray[4]);
         });
 
 
@@ -129,12 +129,12 @@ define(["circuits/ZypSMDReader",
             var methodArray = this.myReader.getMethods();
             //The method properties are defined at the top of b file in the global vars
                
-            assertEquals(5, methodArray.length);
-            assertEquals(myReadCaseList, methodArray[0]);
-            assertEquals(myReadCase, methodArray[1]);
-            assertEquals(myReadCaseNoteList, methodArray[2]);
-            assertEquals(myDeleteCaseNote, methodArray[3]);
-            assertEquals(myReadRawPDF, methodArray[4]);
+            assert.equal(5, methodArray.length);
+            assert.equal(myReadCaseList, methodArray[0]);
+            assert.equal(myReadCase, methodArray[1]);
+            assert.equal(myReadCaseNoteList, methodArray[2]);
+            assert.equal(myDeleteCaseNote, methodArray[3]);
+            assert.equal(myReadRawPDF, methodArray[4]);
         });
 
         
@@ -142,7 +142,7 @@ define(["circuits/ZypSMDReader",
         
         it("Verify if the method returned from a call to getMethod() is identical (same properties) to the corresponding method in the schema", function () {
             var myReadCaseMethod = this.myReader.getMethod("readCase");    
-            assertEquals(myReadCase, myReadCaseMethod);
+            assert.equal(myReadCase, myReadCaseMethod);
         });
 
 
@@ -153,16 +153,16 @@ define(["circuits/ZypSMDReader",
             myReadCaseParamNames = this.myReader.getParameterNames("readCase"),
             myReadCaseNoteListParamNames = this.myReader.getParameterNames("readCaseNoteList");
             
-            assertEquals(2, myReadCaseListParamNames.length);
-            assertEquals("offset", myReadCaseListParamNames[0]);
-            assertEquals("count", myReadCaseListParamNames[1]);        
-            assertEquals(1, myReadCaseParamNames.length);
-            assertEquals("caseNumber", myReadCaseParamNames[0]);        
-            assertEquals(4, myReadCaseNoteListParamNames.length);
-            assertEquals("caseNumber", myReadCaseNoteListParamNames[0]);
-            assertEquals("documentId", myReadCaseNoteListParamNames[1]);
-            assertEquals("noteName", myReadCaseNoteListParamNames[2]);
-            assertEquals("case", myReadCaseNoteListParamNames[3]);
+            assert.equal(2, myReadCaseListParamNames.length);
+            assert.equal("offset", myReadCaseListParamNames[0]);
+            assert.equal("count", myReadCaseListParamNames[1]);        
+            assert.equal(1, myReadCaseParamNames.length);
+            assert.equal("caseNumber", myReadCaseParamNames[0]);        
+            assert.equal(4, myReadCaseNoteListParamNames.length);
+            assert.equal("caseNumber", myReadCaseNoteListParamNames[0]);
+            assert.equal("documentId", myReadCaseNoteListParamNames[1]);
+            assert.equal("noteName", myReadCaseNoteListParamNames[2]);
+            assert.equal("case", myReadCaseNoteListParamNames[3]);
         });
 
 
@@ -173,9 +173,9 @@ define(["circuits/ZypSMDReader",
             myReadCaseParams = this.myReader.getParameters("readCase"),
             myReadCaseNoteListParams = this.myReader.getParameters("readCaseNoteList");
     
-            assertEquals(myReadCaseList.parameters, myReadCaseListParams);
-            assertEquals(myReadCase.parameters, myReadCaseParams);
-            assertEquals(myReadCaseNoteList.parameters, myReadCaseNoteListParams);
+            assert.equal(myReadCaseList.parameters, myReadCaseListParams);
+            assert.equal(myReadCase.parameters, myReadCaseParams);
+            assert.equal(myReadCaseNoteList.parameters, myReadCaseNoteListParams);
        
             
         });
@@ -185,13 +185,13 @@ define(["circuits/ZypSMDReader",
         
         
         it("Verify if the \"required\" property of each method has the same truth value as the boolean returned by isArgument required when called", function () {
-            assertFalse(this.myReader.isArgumentRequired("readCaseList", "offset"));
-            assertFalse(this.myReader.isArgumentRequired("readCaseList", "count"));
-            assertTrue(this.myReader.isArgumentRequired("readCase", "caseNumber"));
-            assertTrue(this.myReader.isArgumentRequired("readCaseNoteList", "caseNumber"));
-            assertTrue(this.myReader.isArgumentRequired("readCaseNoteList", "documentId"));
-            assertFalse(this.myReader.isArgumentRequired("readCaseNoteList", "noteName"));
-            assertFalse(this.myReader.isArgumentRequired("readCaseNoteList", "case"));
+            assert.isFalse(this.myReader.isArgumentRequired("readCaseList", "offset"));
+            assert.isFalse(this.myReader.isArgumentRequired("readCaseList", "count"));
+            assert.isTrue(this.myReader.isArgumentRequired("readCase", "caseNumber"));
+            assert.isTrue(this.myReader.isArgumentRequired("readCaseNoteList", "caseNumber"));
+            assert.isTrue(this.myReader.isArgumentRequired("readCaseNoteList", "documentId"));
+            assert.isFalse(this.myReader.isArgumentRequired("readCaseNoteList", "noteName"));
+            assert.isFalse(this.myReader.isArgumentRequired("readCaseNoteList", "case"));
         });
 
         
@@ -199,18 +199,18 @@ define(["circuits/ZypSMDReader",
         
         describe("\"getServiceUrlMissingParameter\" function", function () {
             it("missing controlNumber required path param, should fail", function () {
-                assertException("Missing arg to readCase", function () {
+                assert.throw(function () {
                     this.myReader.getServiceUrl("readCase");
-                });
+                }, "Missing arg to readCase");
             });
             
             
             it("missing documentId required URL param, should fail", function () {
-                assertException("Missing arg to readCaseNoteList", function () {
+                assert.throw(function () {
                     this.myReader.getServiceUrl("readCaseNoteList", {
                         controlNumber: "90009528"
                     });
-                });
+                }, "Missing arg to readCaseNoteList");
             });
             
         });
@@ -227,7 +227,7 @@ define(["circuits/ZypSMDReader",
                 noteName: "my note"
             });
             
-            assertEquals(myCaseService.target + "/90009528/notes?documentId=1&noteName=my%20note", path);
+            assert.equal(myCaseService.target + "/90009528/notes?documentId=1&noteName=my%20note", path);
             
             path = this.myReader.getServiceUrl("readCaseNoteList", {
                 caseNumber: "9000/9528",
@@ -236,7 +236,7 @@ define(["circuits/ZypSMDReader",
             });
     
             // todo:
-            //assertEquals(rootPath + "/9000%2F9528/notes?documentId=1&noteName=my%20note", path);
+            //assert.equal(rootPath + "/9000%2F9528/notes?documentId=1&noteName=my%20note", path);
             
             
         });
@@ -252,7 +252,7 @@ define(["circuits/ZypSMDReader",
                 count: 25
             });
             
-            assertEquals(myCaseService.target + "?offset=10&count=25", path);
+            assert.equal(myCaseService.target + "?offset=10&count=25", path);
         });
 
 
@@ -260,9 +260,9 @@ define(["circuits/ZypSMDReader",
         
         
         it("Verify if each of the methods' \"transport\" property from the schema matches with the return value of getMethodTransport() when called", function () {
-            assertEquals(myReadCaseList.transport, this.myReader.getMethodTransport("readCaseList"));
-            assertEquals(myReadCase.transport, this.myReader.getMethodTransport("readCase"));
-            assertEquals(myReadCaseNoteList.transport, this.myReader.getMethodTransport("readCaseNoteList"));
+            assert.equal(myReadCaseList.transport, this.myReader.getMethodTransport("readCaseList"));
+            assert.equal(myReadCase.transport, this.myReader.getMethodTransport("readCase"));
+            assert.equal(myReadCaseNoteList.transport, this.myReader.getMethodTransport("readCaseNoteList"));
         });
 
 
@@ -275,7 +275,7 @@ define(["circuits/ZypSMDReader",
                 caseNumber: "90009528"
             });
 
-            assertEquals("90009528", arg); 
+            assert.equal("90009528", arg); 
            
         });
 
@@ -283,20 +283,20 @@ define(["circuits/ZypSMDReader",
         
         
         it("Verify if an exception is thrown if a parameter with the \"required\" property set to true is missing/undefined.", function () {
-            assertException("Missing required arg not validated", function () {
+            assert.throw(function () {
                 var param = myReadCase.parameters[0],
                     arg = this.myReader.getAndValidateArgument(param, {});
-            });
+            }, "Missing required arg not validated");
     
-            assertException("Missing required arg not validated", function () {
+            assert.throw(function () {
                 var param = myReadCaseNoteList.parameters[0],
                     arg = this.myReader.getAndValidateArgument(param, {});
-            });
+            }, "Missing required arg not validated");
             
-            assertException("Missing required arg not validated", function () {
+            assert.throw( function () {
                 var param = myReadCaseNoteList.parameters[1],
                     arg = this.myReader.getAndValidateArgument(param, {});
-            });
+            }, "Missing required arg not validated");
         });
 
 
@@ -311,7 +311,7 @@ define(["circuits/ZypSMDReader",
                 caseNumber: "90009528",
                 documentId: 17
             });
-            assertEquals(expectedUrl, newUrl);    
+            assert.equal(expectedUrl, newUrl);    
         });
 
 
@@ -328,7 +328,7 @@ define(["circuits/ZypSMDReader",
                 documentId: 17
             });
                     
-            assertEquals(expectedUrl, newUrl);
+            assert.equal(expectedUrl, newUrl);
         });
 
 
@@ -338,14 +338,14 @@ define(["circuits/ZypSMDReader",
             //expects a "service" object
             var params = this.myReader.enumerateParameters(resolvedCaseService.services.readCaseNoteList);
     
-            assertEquals(1, params.PATH.length);
-            assertEquals(params.PATH[0], myReadCaseNoteList.parameters[0]);
-            assertEquals(2, params.URL.length);
-            assertEquals(params.URL[0], myReadCaseNoteList.parameters[1]);
-            assertEquals(params.URL[1], myReadCaseNoteList.parameters[2]);
-            assertEquals(1, params.JSON.length);
-            assertEquals(params.JSON[0], myReadCaseNoteList.parameters[3]);
-            assertEquals(0, params.ENTITY.length);
+            assert.equal(1, params.PATH.length);
+            assert.equal(params.PATH[0], myReadCaseNoteList.parameters[0]);
+            assert.equal(2, params.URL.length);
+            assert.equal(params.URL[0], myReadCaseNoteList.parameters[1]);
+            assert.equal(params.URL[1], myReadCaseNoteList.parameters[2]);
+            assert.equal(1, params.JSON.length);
+            assert.equal(params.JSON[0], myReadCaseNoteList.parameters[3]);
+            assert.equal(0, params.ENTITY.length);
         });
 
 
@@ -354,7 +354,7 @@ define(["circuits/ZypSMDReader",
         
         it("expects a \"service\" object", function () {
             var params = gpReader.enumerateParameters(gpReader.smd.services.readModel);
-            assertEquals(1, params.PATH.length);       
+            assert.equal(1, params.PATH.length);       
         });
 
 
@@ -362,9 +362,9 @@ define(["circuits/ZypSMDReader",
         
         
         it("Verify if the object returned by getResponseSchema() matches the object in the \"returns\" field in the schema of the passed method", function () {
-            assertEquals(this.myReader.getResponseSchema("readCaseList"), myReadCaseList.returns);
-            assertEquals(this.myReader.getResponseSchema("readCase"), myReadCase.returns);
-            assertEquals(this.myReader.getResponseSchema("readCaseNoteList"), myReadCaseNoteList.returns);
+            assert.equal(this.myReader.getResponseSchema("readCaseList"), myReadCaseList.returns);
+            assert.equal(this.myReader.getResponseSchema("readCase"), myReadCase.returns);
+            assert.equal(this.myReader.getResponseSchema("readCaseNoteList"), myReadCaseNoteList.returns);
         });
         
         
@@ -373,10 +373,10 @@ define(["circuits/ZypSMDReader",
         
         
         it("Test the Zyp SMD reader's \"getResponsePayloadName\" function", function () {
-            assertEquals("cases", this.myReader.getResponsePayloadName("readCaseList"));
-            assertEquals("case", this.myReader.getResponsePayloadName("readCase"));
+            assert.equal("cases", this.myReader.getResponsePayloadName("readCaseList"));
+            assert.equal("case", this.myReader.getResponsePayloadName("readCase"));
             //this last one is omitted on the method and pulled from the service root
-            assertEquals("cases", this.myReader.getResponsePayloadName("readCaseNoteList"));
+            assert.equal("cases", this.myReader.getResponsePayloadName("readCaseNoteList"));
         });
 
 
@@ -384,7 +384,7 @@ define(["circuits/ZypSMDReader",
         
         
         it("Verifty the correct payload model", function () {
-            assertEquals("model", sReader.getRequestPayloadName("createModel"));
+            assert.equal("model", sReader.getRequestPayloadName("createModel"));
         });
 
 
@@ -392,9 +392,9 @@ define(["circuits/ZypSMDReader",
         
         
         it("Verify if the isListResponse() function correctly returns whether or not the response payload type is array", function () {
-            assertFalse(this.myReader.isListResponse("readCase"));
-            assertTrue(this.myReader.isListResponse("readCaseList"));
-            assertTrue(this.myReader.isListResponse("readCaseNoteList"));
+            assert.isFalse(this.myReader.isListResponse("readCase"));
+            assert.isTrue(this.myReader.isListResponse("readCaseList"));
+            assert.isTrue(this.myReader.isListResponse("readCaseNoteList"));
         });
 
 
@@ -404,7 +404,7 @@ define(["circuits/ZypSMDReader",
         it("Verify if parameters in a base schema are available to the derived.", function () {
             var params = sReader.getParameters("readModel");
     
-            assertEquals(3, params.length);
+            assert.equal(3, params.length);
         });
 
 
@@ -416,12 +416,12 @@ define(["circuits/ZypSMDReader",
             params2 = mReader.getParameters("simpleService"),
             params3 = mReader.getParameters("withEnvelope");
             
-            assertNotUndefined(params[2].envelope);
-            assertEquals("PATH", params[2].envelope);        
-            assertNotUndefined(params2[0].envelope);
-            assertEquals("URL", params2[0].envelope);        
-            assertNotUndefined(params3[0].envelope);
-            assertEquals("JSON", params3[0].envelope);
+            assert.isDefined(params[2].envelope);
+            assert.equal("PATH", params[2].envelope);        
+            assert.isDefined(params2[0].envelope);
+            assert.equal("URL", params2[0].envelope);        
+            assert.isDefined(params3[0].envelope);
+            assert.equal("JSON", params3[0].envelope);
         });
     });
    
