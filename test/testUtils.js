16fbeb52bc3c9245390ee149a8b9e608b5d02b15
David Tittsworth <stopyoukid@gmail.com>
2015-09-17 10:44:51 -0700

    Comment updates

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,8 +1,11 @@
 define([],
     function() {
         return {
+
             /**
              * Asserts that two arrays are equal deeply
+             * @param {Array} arr The first array to compare
+             * @param {Array} arr2 The second array to compare
              */
             arrayEquals: function(arr, arr2) {
                 assert.equal(arr.length, arr2.length, "Array lengths differ");
@@ -10,6 +13,12 @@ define([],
                     assert.equal(item, arr2[i]);
                 });
             },
+
+            /**
+             * Asserts that when the given function is run, it throws an exception with a message containing the given message
+             * @param {String} message The expected message
+             * @param {Function} exFunc The function that should thrown an exception
+             */
             assertException: function(message, exFunc) {
                 try {
                     exFunc();
@@ -21,7 +30,7 @@ define([],
 
             /**
              * Provides a very basic clone method
-             *
+             * @param {Object} e The object to clone
              */
             clone: function(e) {
                 return JSON.parse(JSON.stringify(e));
